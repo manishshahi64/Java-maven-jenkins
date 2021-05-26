ecd2787666cf0e93bbf21c2006e862885d1ff006
manishshahi64 <manishshahi128@gmail.com>
2021-05-26 20:20:39 +0530

    Editing the test file

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
   
   @Test
   public void greetShouldIncludeGreeting() {
-    String someone = "Hello World by Manish";
+    String someone = "Hello World by Manish Shahi";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
